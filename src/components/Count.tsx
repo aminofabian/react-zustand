22d80887aec57642afca77e6abc925b6c34d6fbb
aminofabian <aminofab@gmail.com>
2024-05-06 18:32:26 +0300

    added async

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -2,14 +2,14 @@ import { useCounterStore } from "../store"
 
 const Count = () => {
   const count = useCounterStore((state) => state.count)
-  const increment = useCounterStore((state) => state.increment)
+  const incrementAsync = useCounterStore((state) => state.incrementAsync)
   const decrement = useCounterStore((state)=> state.decrement)
   
   return (
     <div>{count}
     
     <div>
-    <div><button onClick={increment}>Increment</button></div>
+    <div><button onClick={incrementAsync}>incrementAsync</button></div>
     <div><button onClick={decrement}> Decrement</button></div>
     </div>
     
